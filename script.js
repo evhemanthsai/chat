df8c94c0a0af7ffa5bdb55a00e5d2ac60c8829b0
E-V-HEMANTH <74523620+evhemanthsai@users.noreply.github.com>
2023-02-04 18:07:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,13 +15,44 @@ form.addEventListener("submit", e => {
   const messageElement = document.createElement("p");
   messageElement.textContent = message;
   chatArea.appendChild(messageElement);
+
+  fetch("https://api.github.com/repos/{your_username}/{your_repo}/contents/data.json", {
+    method: "PUT",
+    headers: {
+      "Authorization": "Bearer {your_token}",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      message: messages
+    })
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Failed to save data");
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
 });
 
 window.addEventListener("load", () => {
-  const messages = JSON.parse(localStorage.getItem("messages")) || [];
-  messages.forEach(message => {
-    const messageElement = document.createElement("p");
-    messageElement.textContent = message;
-    chatArea.appendChild(messageElement);
-  });
+  fetch("https://api.github.com/repos/{your_username}/{your_repo}/contents/data.json")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Failed to load data");
+      }
+      return response.json();
+    })
+    .then(data => {
+      const messages = JSON.parse(atob(data.content));
+      messages.forEach(message => {
+        const messageElement = document.createElement("p");
+        messageElement.textContent = message;
+        chatArea.appendChild(messageElement);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
 });
